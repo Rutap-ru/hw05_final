ab701a3e6eb439cb15e475b985f770df20185ed0
Nikolay <rutap@yandex.ru>
2021-01-29 19:58:29 +0700

    Sprint-6 / PASSED 100%

--- yatube/urls.py
+++ yatube/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.urls import include, path
 
-handler404 = "posts.views.page_not_found"
-handler500 = "posts.views.server_error"
+handler404 = "posts.views.page_not_found" # noqa
+handler500 = "posts.views.server_error" # noqa
 
 urlpatterns = [
     path('auth/', include('users.urls')),
